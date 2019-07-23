878a4a9038a25f92937282f7d4c2a9ad8877595b
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-07-23 22:34:26 +0300

    add lastElement

---
+++ lastElemnts.py
@@ -0,0 +1,9 @@
+from collections import deque
+
+
+def search(lines, pattern, history=3):
+    prev_lines = deque(maxlen=history)
+    for line in lines:
+        if pattern in line:
+            yield line, prev_lines
+        prev_lines.append(line)
