c4f201adcfebc6007bb563cc88a954acd3a1aa92
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-09-10 22:49:27 +0300

    speech module v0.1

---
+++ speech_module.py
@@ -0,0 +1,28 @@
+FIRST_TEN = ["one", "two", "three", "four", "five", "six", "seven",
+             "eight", "nine"]
+SECOND_TEN = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+              "sixteen", "seventeen", "eighteen", "nineteen"]
+OTHER_TENS = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy",
+              "eighty", "ninety"]
+HUNDRED = "hundred"
+
+
+def checkio(number):
+    number = str(number)
+    speech = ''
+    if len(number) == 3:
+        print(number,'hundred')
+    if len(number) == 2:
+        print(number,'OTHER_TENS')
+    if len(number) == 1:
+        print(number)
+  
+    #replace this for solution
+    return 'string representation of n'
+
+
+checkio(4)
+checkio(14)
+checkio(234)
+checkio(12)
+checkio(40)
