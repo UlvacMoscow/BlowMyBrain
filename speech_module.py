a23650fdb138465f3f55cd951ec2a3965021bf65
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-09-18 23:15:45 +0300

    speech module well done

--- speech_module.py
+++ speech_module.py
@@ -12,24 +12,29 @@ SECOND_TEN_DICT = { x+10:SECOND_TEN[x] for x in range(0,len(SECOND_TEN))}
 OTHER_TENS_DICT = { ((x+2)*10):OTHER_TENS[x] for x in range(0,len(OTHER_TENS))}
 
 def checkio(number):
-    test = { x + 1:FIRST_TEN[x] for x in range(0,len(FIRST_TEN))}
+    speech = ''
     hundred = number // 100
     if hundred > 0:
+        speech +='{} {} '.format(FIRST_TEN_DICT[hundred], HUNDRED) 
         if number % 100 == 0:
-            return print()
-        number = number - hundred * 100
-
-
+            return print(speech.strip())
+        else:
+            number = number - hundred * 100
+    if number in SECOND_TEN_DICT:
+        speech += '{}'.format(SECOND_TEN_DICT[number])
+        return print(speech.strip())
     tens = number // 10
-    if tens > 0:
-        num = number - tens * 10
+    if tens > 1:
+        tens = tens * 10
+        speech += '{}'.format(OTHER_TENS_DICT[tens])
+        if number % 10 == 0:
+            return print(speech.strip())
+        number -= tens
+        speech += ' {}'.format(FIRST_TEN_DICT[number])
     else:
-        num = number
+        speech += '{}'.format(FIRST_TEN_DICT[number])
 
-  print(test)
-
-    #replace this for solution
-    return 'string representation of n'
+    return print(speech.strip())
 
 
 checkio(4)
