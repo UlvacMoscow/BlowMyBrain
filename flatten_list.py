2a6a50e9d7a7040438d8b14f1df127a85558c886
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-10-10 22:40:13 +0300

    update i like this solution

--- flatten_list.py
+++ flatten_list.py
@@ -4,12 +4,17 @@ a = [1,[3,3,[4,5],3],2]
 d=[]
 
 def flatten(arr):
-    for x in arr:
-        if isinstance(x, list):
-            flatten(x)
-        else:
-            d.append(x)
-    d.sort()
-    return print(d)
+    d = []
+    def loop(c):
+        for x in c:
+            if isinstance(x, list):
+                loop(x)
+            else:
+                d.append(x)
+    loop(arr)
+    return sorted(d)
 
-flatten(a)
+print(flatten([1, 2, 3]))
+print(flatten([1, [2, 2, 2], 4]))
+print(flatten([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]))
+print(flatten([-1, [1, [-2], 1], -1]))
