7dc63fa252c923f27404e123105edee7834ae8d0
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-10-10 21:57:47 +0300

    update list so write full list, rename to flatten

--- flatten_list.py
+++ flatten_list.py
@@ -1,25 +1,15 @@
-import copy
-
 
 a = [1,[3,3,[4,5],3],2]
 # c = [a.append(a.pop(a.index(x))) for x in a if isinstance(x, list)]
 d=[]
-e = []
-def conv(cp):
-    c = copy.deepcopy(cp)
-    for x in c:
-        print('x1 ',x)
+
+def flatten(arr):
+    for x in arr:
         if isinstance(x, list):
-            print('list')
-            b = cp.pop(cp.index(x))
-            conv(b)
+            flatten(x)
         else:
-            print('no list')
-
             d.append(x)
-            print('x2 ',x)
+    d.sort()
+    return print(d)
 
-    # return d
-
-e = conv(a)
-print(d)
+flatten(a)
