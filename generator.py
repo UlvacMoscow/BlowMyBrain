f436fa37469f82dbb6e1cb2a58d781e350ff2e21
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-10-22 21:59:42 +0300

    return val

--- generator.py
+++ generator.py
@@ -1,11 +1,11 @@
 def simple_generator(val):
     while val > 0:
         val -= 1
-        yield 1
+        yield val
 
 
 gen_it = simple_generator(4)
-print(next(gen_it))
+print(type(gen_it))
 print(next(gen_it))
 print(next(gen_it))
 print(next(gen_it))
