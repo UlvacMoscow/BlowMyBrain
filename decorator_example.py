7c481562718fa9e0763a797333ed827ea9daafdf
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2019-10-28 22:58:06 +0300

    simple custom decorator

---
+++ decorator_example.py
@@ -0,0 +1,15 @@
+def simple_decorator(func_to_decorate):
+    def wrapper():
+        print('code before original function')
+        func_to_decorate()
+        print('code after original function')
+    return wrapper
+
+
+def original_function():
+    print('i am original')
+
+
+
+decorate_original_function = simple_decorator(original_function)
+decorate_original_function()
